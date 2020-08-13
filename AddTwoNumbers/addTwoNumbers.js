eb503b0e5d0d60b8e8749f64c4dbe07a2ad8429a
elmarek <mareks@gmx.com>
2020-08-13 12:30:24 -0700

    add two numbers non working function

--- AddTwoNumbers/addTwoNumbers.js
+++ AddTwoNumbers/addTwoNumbers.js
@@ -11,3 +11,63 @@ Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 
 */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+var addTwoNumbers = function (l1, l2) {
+  // create array to store values
+  let numArray = [];
+
+  // create function to traverse linked lists and add values to array
+  let traverser = (node) => {
+    // counter variable to keep track of numArray index
+    let counter = 0;
+    while (node.next) {
+      if (numArray[counter] === undefined) {
+        numArray.push(node.value);
+      } else {
+        numArray[counter] = numArray[counter] + node.value;
+      }
+      counter++;
+    }
+  };
+  // instantiate traverser function for each node value
+  traverser(l1);
+  traverser(l2);
+
+  // iterate through the numArray checking for any values greater than 10
+  // if value is greater than 10, carry over the remainder to next index
+  for (var i = 0; i < numArray.length; i++) {
+    if (numArray[i] > 9) {
+      numArray[i] = numArray[i] % 10;
+      if (numArray[i + 1] === undefined) {
+        numArray[i + 1] = 1;
+      } else {
+        numArray[i + 1] = numArray[i + 1] + 1;
+      }
+    }
+  }
+
+  // we now have an array of values that need to be added to new linked list
+
+  function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+
+  //create the last node with next value of null and value of                          numArray[numArray.length - 1]
+
+  // create variable that points to list node
+  var i = numArray.length - 1;
+  var list = null;
+  while (i >= 0) {
+    list = new ListNode(numArray[i], list);
+    i--;
+  }
+  return list;
+};
