fa3487cf476d708d32f9622dcc4d84761b8a779e
elmarek <mareks@gmx.com>
2020-08-17 10:23:18 -0700

    Initial solution for reorderDataInLogFiles

--- reorderDataInLogFiles/reorderDataInLogFiles.js
+++ reorderDataInLogFiles/reorderDataInLogFiles.js
@@ -20,3 +20,22 @@ Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3
 Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
 
 */
+
+var reorderLogFiles = function (logs) {
+  let letters = [];
+  let digits = [];
+
+  for (var i = 0; i < logs.length; i++) {
+    let spaced = logs[i].split("");
+
+    if (!Number(spaced[5])) {
+      let array = spaced.join("");
+      letters.push(logs[i]);
+    } else {
+      let array = spaced.join("");
+      digits.push(array);
+    }
+  }
+  let result = letters.concat(digits);
+  return result;
+};
