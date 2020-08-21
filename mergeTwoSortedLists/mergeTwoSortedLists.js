9437b381717ba16915b23096506c6ed3ddf94ef6
elmarek <mareks@gmx.com>
2020-08-21 15:19:11 -0700

    initial commit for merge two sorted lists

---
+++ mergeTwoSortedLists/mergeTwoSortedLists.js
@@ -0,0 +1,8 @@
+var mergeTwoLists = function (l1, l2) {
+  // create empty merged list object
+  // traverse l1
+  // compare current value of l1 vs l2
+  // add and delete lowest value to mergedList
+  // Repeat until lists are empty
+  // return merged list
+};
