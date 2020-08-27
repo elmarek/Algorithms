65b3657683289f1aa518323a0f8cc60c84277551
elmarek <mareks@gmx.com>
2020-08-27 12:45:23 -0700

    Working solution for valid parens

---
+++ validParens/validParens.js
@@ -0,0 +1,64 @@
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+*/
+
+var isValid = function (s) {
+  if (s.length === 0) {
+    return true;
+  } else if (s.length === 1) {
+    return false;
+  }
+
+  var dictionary = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  // split the string
+  let isValid = true;
+  let split = s.split("");
+
+  // iterate over the array
+  for (var i = 0; i < split.length; i++) {
+    //iterate through ineer values
+    for (var j = i + 1; j < split.length; j++) {
+      console.log(split);
+      if (
+        dictionary[split[j]] &&
+        dictionary[split[j]] === split[j + 1] &&
+        split.length > 1
+      ) {
+        split.splice(j, 2);
+        j -= 2;
+        if (split.length === 0) {
+          return isValid;
+        }
+      }
+    }
+
+    // if the next or last are closing parens take them out of array
+    if (dictionary[split[i]] === split[i + 1] && split.length > 1) {
+      split.shift();
+      split.shift();
+      i--;
+    } else if (
+      dictionary[split[i]] === split[split.length - 1] &&
+      split.length > 1
+    ) {
+      split.shift();
+      split.pop();
+      i--;
+    } else {
+      isValid = false;
+      break;
+    }
+  }
+  return isValid;
+};
