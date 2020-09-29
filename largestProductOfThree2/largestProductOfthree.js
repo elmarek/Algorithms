3bbbfcc2855630471295d009faf0b10a66bcf913
elmarek <mareks@gmx.com>
2020-09-29 10:44:26 -0700

    Solution for  largestProductOfThree

--- largestProductOfThree2/largestProductOfthree.js
+++ largestProductOfThree2/largestProductOfthree.js
@@ -2,48 +2,41 @@
  * from an array.
  *
  * Extra credit: Make your function handle negative numbers.
-
-
-var largestProductOfThree = function(array) {
-  # /* Write a function that finds the largest possible product of any three numbers
-  #  * from an array.
-  #  *
-  #  * Extra credit: Make your function handle negative numbers.
-  #
-  # O: the number
-  # I: array (not sure if we will get things that are not numbers)
-  # C:
-  # E:
-  */
-  def largest_product_of_three(arr):
-
-       sorted_array = sorted(arr, key=None, reverse=True)
-      product = sorted_array[0] * sorted_array[1] * sorted_array[2]
-      return product
-  def largest_product_of_three_with_negative(arr):
-     let hasPositives = false
-     let hasNegatives = false
-     let mixed = false
-     for (var i = 0; i < arr.length; i++) {
-         if (arr[i] === 0) {
-             continue
-         }
-         if(arr[i] > 0) {
-             hasPositives = true
-         } else if (arr[i] < 0) {
-             hasNegatives = true
-         }
-     }
-     if (hasPositives === true && hasNegatives === true) {
-         mixed = true
-     }
-     if (mixed) {
-       return arr[0] * arr[arr.length -1] * arr[arr.length - 2]
-     }
-     if (hasNegatives) {
-     }
-     if(hasPositives) {
-     }
-      return product
-
+ */
+var largestProductOfThree = function (arr) {
+  let hasPositives = false;
+  let hasNegatives = false;
+  let mixed = false;
+  arr = arr.sort(function (a, b) {
+    return b - a;
+  });
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      continue;
+    }
+    if (arr[i] > 0) {
+      hasPositives = true;
+    } else if (arr[i] < 0) {
+      hasNegatives = true;
+    }
+  }
+  if (hasPositives === true && hasNegatives === true) {
+    mixed = true;
+  }
+  if (mixed) {
+    var lastTwo = Math.abs(arr[arr.length - 1] * arr[arr.length - 2]);
+    var twoThree = Math.abs(arr[1] * arr[2]);
+    if (lastTwo >= twoThree) {
+      return arr[0] * lastTwo;
+    }
+    if (lastTwo < twoThree) {
+      return arr[0] * twoThree;
+    }
+  }
+  if (hasNegatives) {
+    return arr[0] * arr[1] * arr[2];
+  }
+  if (hasPositives) {
+    return arr[0] * arr[1] * arr[2];
+  }
 };
