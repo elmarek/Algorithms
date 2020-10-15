65970c3ad7f444b4ab42d7c1772ed6e3d34366b7
elmarek <mareks@gmx.com>
2020-10-15 09:06:51 -0700

    comments for trapping rainwater 2

---
+++ trappingRainwater2/trappingRainwater2.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+
+/* 42. Trapping Rain Water
+https://leetcode.com/problems/trapping-rain-water/
+
+Helpful animation of this prompt: https://youtu.be/HmBbcDiJapY?t=51
+
+
+Given n non-negative integers representing an elevation map where
+the width of each bar is 1, compute how much water it is able to trap
+after raining.
+
+
+VIEW ELEVATION MAP ON LEETCODE
+
+
+
+Example:
+
+Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+
+*/
+
+
+var trap = function(height) {
+
+};
