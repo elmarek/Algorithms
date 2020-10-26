8371b0422ee2d8b1aa474325eabaf20df9d793d0
elmarek <mareks@gmx.com>
2020-10-26 10:54:51 -0700

    Solution for convert-binary-number-in-a-linked-list-to-integer

--- binaryToIntLinkedList/binaryToIntLinkedList.js
+++ binaryToIntLinkedList/binaryToIntLinkedList.js
@@ -46,6 +46,28 @@ Constraints:
 
 */
 
-var binaryToIntLinkedList = function(head) {
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
 
+var getDecimalValue = function(head) {
+  // extract values to array
+  let arr = []
+  while(head) {
+      arr.push(head.val)
+      head = head.next
+  }
+  let output = 0
+  for (var i = 0; i < arr.length; i++) {
+      output += arr[i] * (2**(arr.length - (1 + i)))
+  }
+  return output
 };
