f6e0d9841f20d8b90bb548cb43f2b9c103a72bc6
elmarek <mareks@gmx.com>
2020-11-04 11:20:02 -0700

    Comments fort palindrome linked list

---
+++ palindromeLinkedList/palindromeLinkedList.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+ /* 234. Palindrome Linked List
+ https://leetcode.com/problems/palindrome-linked-list/
+
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+Follow up:
+Could you do it in O(n) time and O(1) space?
+*/
+
+
+var isPalindrome = function(head) {
+
+};
