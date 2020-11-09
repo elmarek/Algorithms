45408fd4d07b094d9f3c9c5f6d7fda6f581e27e2
elmarek <mareks@gmx.com>
2020-11-09 11:05:22 -0700

    Solution for productSum

---
+++ productSum/productSum.js
@@ -0,0 +1,14 @@
+// Algo expert problem
+
+function productSum(array, multiplier = 1) {
+  // Write your code here.
+	let sum = 0
+  for (var i = 0; i < array.length; i++) {
+	  if (Array.isArray(array[i])) {
+  	  sum += productSum(array[i], multiplier + 1)
+		} else {
+			sum += array[i]
+		}
+	}
+  return sum * multiplier
+}
