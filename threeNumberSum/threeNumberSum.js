c62f0ae0254254cfdc07f8bc533ddefd99d8c818
elmarek <mareks@gmx.com>
2020-11-16 10:49:42 -0700

    Solution  Three Number Sum

--- threeNumberSum/threeNumberSum.js
+++ threeNumberSum/threeNumberSum.js
@@ -1,22 +1,21 @@
 function threeNumberSum(array, targetSum) {
   // Write your code here.
 	// sort the array
-	let sorted = array.sort((a ,b) => {return a - b})
-	console.log(sorted)
+	array.sort((a ,b) => a - b)
 	let triplets = []
-	for (var i = 0; i < array.length - 2; i++) {
-		let left = i + i
+	for (let i = 0; i < array.length - 2; i++) {
+		let left = i + 1
 		let right = array.length - 1
 		while (left < right) {
-			let currentSum = array[i] + array[left] + array[right]
+			const currentSum = array[i] + array[left] + array[right]
 			if (currentSum === targetSum) {
 				triplets.push([array[i], array[left], array[right]])
-				left += 1
-				right -= 1
+				left++
+				right--
 			} else if (currentSum < targetSum) {
-				left += 1
+				left++
 			} else if (currentSum > targetSum) {
-				right -= 1
+				right--
 			}
 		}
 	}
