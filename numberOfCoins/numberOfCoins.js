feb1aa3ef4d8d59ba666fdf09fb9d72fb82e9ad6
elmarek <mareks@gmx.com>
2020-12-03 11:52:50 -0700

    Solution for Number of coins- DP

---
+++ numberOfCoins/numberOfCoins.js
@@ -0,0 +1,21 @@
+/*
+
+Algo Expert Problem
+
+*/
+
+function minNumberOfCoinsForChange(n, denoms) {
+  let numOfCoins = new Array(n + 1).fill(Infinity);
+  numOfCoins[0] = 0;
+  for (const denom of denoms) {
+    for (let amount = 0; amount < numOfCoins.length; amount++) {
+      if (denom <= amount) {
+        numOfCoins[amount] = Math.min(
+          numOfCoins[amount],
+          1 + numOfCoins[amount - denom]
+        );
+      }
+    }
+  }
+  return numOfCoins[n] === Infinity ? -1 : numOfCoins[n];
+}
