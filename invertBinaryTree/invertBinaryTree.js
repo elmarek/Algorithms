72f9cd57b3bdc34c89d1c98d7044f25f9ef94a2d
elmarek <mareks@gmx.com>
2020-12-11 13:18:51 -0700

    Solution for invert binary tree

--- invertBinaryTree/invertBinaryTree.js
+++ invertBinaryTree/invertBinaryTree.js
@@ -16,3 +16,31 @@ Output:
  / \   / \
 9   6 3   1
 */
+
+function invertBinaryTree(tree) {
+  // Write your code here.
+  let queue = [tree];
+  while (queue.length) {
+    const current = queue.shift();
+    if (current === null) {
+      continue;
+    }
+    swap(current);
+    queue.push(current.left);
+    queue.push(current.right);
+  }
+}
+
+function swap(tree) {
+  let left = tree.right;
+  tree.right = tree.left;
+  tree.left = left;
+}
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
